759eceb3d47313bb47d82293d99fd8b68d7b58be
IT-Gleb <mygit85@gmail.com>
2026-05-23 18:08:23 +0300

    @internationalized/daye library installed

--- src/entityes/components/calendar/CalendarBasic.tsx
+++ src/entityes/components/calendar/CalendarBasic.tsx
@@ -1,15 +1,44 @@
 "use client";
 
 import { Calendar } from "@heroui/react";
-import { FC } from "react";
+import { FC, useEffect, useMemo, useState } from "react";
 
-interface CalendarBasicProps {}
+interface CalendarBasicProps {
+  calendar: { identifier: string };
+  day: number;
+  era: string;
+  month: number;
+  year: number;
+}
 
-const CalendarBasic: FC<CalendarBasicProps> = () => {
+type TCalendarProps = CalendarBasicProps;
+
+const CalendarBasic: FC = ({ param }: { param?: TCalendarProps }) => {
+  const dateValue: CalendarBasicProps = {
+    calendar: {
+      identifier: "gregory",
+    },
+    day: 25,
+    era: "AD",
+    month: 5,
+    year: 2026,
+  };
+  const [value, setValue] = useState<any | null>(null);
+  const [focusedDate, setFocusedDate] = useState<any>(value);
+
+  const val = useMemo(() => {
+    console.log(value);
+    return value;
+  }, [value]);
+
   return (
     <Calendar
       aria-label="Event date"
       className={" p-2 border border0slate-400/35"}
+      focusedValue={focusedDate}
+      value={value}
+      onChange={setValue}
+      onFocusChange={setFocusedDate}
     >
       <Calendar.Header>
         <Calendar.Heading />
