312ed8037f7071d8e934589b65ebe671a197c4c4
IT-Gleb <mygit85@gmail.com>
2026-07-02 10:22:26 +0300

    updt: hero Component updated

--- src/app/(isShop)/shop/page.tsx
+++ src/app/(isShop)/shop/page.tsx
@@ -1,13 +1,5 @@
+import HeroComp from "@/entityes/components/shop/mainPage/heroComp";
+
 export default async function ShopPage() {
-  return (
-    <div className="mt-5 w-full min-h-100 bg-green-800 dark:bg-blue-900 transition-discrete duration-300 rounded-t-3xl overflow-hidden">
-      <div className="w-full h-auto bg-repeat bg-size-[14px_16px] lg:bg-size-[25px_25px] bg-[linear-gradient(rgba(125,125,125,0.1)_1px,transparent_1px),linear-gradient(90deg,rgba(155,125,125,0.1)_1px,transparent_1px)]">
-        <div className="ml-auto w-55 h-55 bg-radial-[at_100%_0%] from-green-300/45 dark:from-blue-300/50 to-transparent to-72%"></div>
-        <h4 className="w-fit mx-auto p-1 uppercase text-3xl font-semibold text-amber-100 dark:text-amber-300">
-          Привет всем! Народ налетай, подешевело...
-        </h4>
-        <div className="mr-auto w-55 h-55 bg-radial-[at_0%_100%] from-green-300/45 dark:from-blue-300/50 to-transparent to-72%"></div>
-      </div>
-    </div>
-  );
+  return <HeroComp text="Приветствуем вас в нашем магазине" />;
 }
---
+++ src/entityes/components/shop/mainPage/heroComp.tsx
@@ -0,0 +1,131 @@
+"use client";
+import { useMediaQuery } from "@heroui/react";
+import { stagger } from "motion";
+import * as motion from "motion/react-client";
+import { useMemo, useState } from "react";
+
+const images1 = [
+  "/images/pirozhki-1.jpg",
+  "/images/pirozhki-2.jpg",
+  "/images/pirozhki-3.jpg",
+  "/images/pirozhki-4.jpg",
+];
+
+const images2 = [
+  "/images/desert-1.jpg",
+  "/images/desert-3.jpg",
+  "/images/desert-4.jpg",
+  "/images/desert-7.jpg",
+];
+
+const ImageAnimo = {
+  active: {
+    x: [-1200, 120, 0],
+    transition: { delay: stagger(0.35, { startDelay: 0.2 }) },
+  },
+};
+
+const RoundImage = ({
+  url,
+  borderColor,
+  index = 0,
+  direction_animate,
+}: {
+  url: string;
+  borderColor: string;
+  index: number;
+  direction_animate: "right" | "top";
+}) => {
+  return direction_animate === "right" ? (
+    <motion.div
+      className={`w-28 h-28 rounded-full overflow-hidden object-cover object-center p-0 border-6 ${borderColor}`}
+      initial={{ x: -1200 }}
+      animate={{ x: [-1200, 120, 0] }}
+      transition={{ delay: 0.75 * index }}
+    >
+      <img
+        src={url}
+        loading="lazy"
+        fetchPriority="high"
+        decoding="async"
+        className="w-full h-full"
+      />
+    </motion.div>
+  ) : (
+    <motion.div
+      className={`w-36 h-28 rounded-2xl overflow-hidden object-cover object-center shadow-lg border-6 ${borderColor}`}
+      initial={{ y: -1000 }}
+      animate={{ y: [1000, -120, 0] }}
+      transition={{ delay: 0.5 * index }}
+    >
+      <img
+        src={url}
+        loading="lazy"
+        fetchPriority="high"
+        decoding="async"
+        className="w-full h-full"
+      />
+    </motion.div>
+  );
+};
+
+const HeroComp = ({ text }: { text: string }) => {
+  const isMobile = useMediaQuery(" screen and (200px < width <= 768px)");
+
+  const img1 = useMemo<string[]>(() => {
+    if (isMobile) {
+      return images1.slice(0, 3);
+    }
+    return images1;
+  }, [isMobile]);
+
+  const img2 = useMemo<string[]>(() => {
+    if (isMobile) {
+      return images2.slice(0, 2);
+    }
+    return images2;
+  }, [isMobile]);
+
+  return (
+    <div className="mt-5 w-full min-h-100 bg-green-800 dark:bg-blue-900 transition-discrete duration-300 rounded-t-3xl overflow-hidden">
+      <div className="w-full h-auto backlines relative">
+        <div className="absolute left-1 sm:left-10 top-10 flex gap-x-4 flex-nowrap items-center justify-evenly">
+          {img1
+            .map((item, index) => (
+              <RoundImage
+                key={item}
+                url={item}
+                borderColor="border-green-400/50 dark:border-blue-400/50"
+                index={index}
+                direction_animate="right"
+              />
+            ))
+            .reverse()}
+        </div>
+        <div className="ml-auto w-55 h-55 bg-radial-[at_100%_0%] from-green-300/30 dark:from-blue-300/25 to-transparent to-72%"></div>
+        <motion.h4
+          className="w-fit mx-auto p-1 uppercase text-2xl lg:text-4xl font-semibold text-amber-100 dark:text-amber-300"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 2.5, ease: "easeInOut", duration: 0.5 }}
+        >
+          {text}
+        </motion.h4>
+        <div className="absolute right-2 lg:right-10 bottom-10 flex gap-x-4 flex-nowrap items-center justify-evenly">
+          {img2.map((item, index) => (
+            <RoundImage
+              key={item}
+              url={item}
+              borderColor="border-green-400/50 dark:border-blue-400/50"
+              index={index}
+              direction_animate="top"
+            />
+          ))}
+        </div>
+        <div className="mr-auto w-55 h-55 bg-radial-[at_0%_100%] from-green-300/30 dark:from-blue-300/25 to-transparent to-72%"></div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroComp;
