f427146a7f6d7a2a7b9df12c4a85a0317174b1e7
IT-Gleb <mygit85@gmail.com>
2026-07-31 08:59:10 +0300

    update order status route modify

--- src/app/api/updateOrderStatus/route.ts
+++ src/app/api/updateOrderStatus/route.ts
@@ -29,15 +29,29 @@ export async function POST(request: Request) {
     }),
   );
 
-  await Promise.allSettled(fetchArray).then((data) =>
-    data.forEach((item) => {
-      if (item.status === "rejected") {
-        // console.log(item.value.ok);
-        isError = true;
-      }
-    }),
-  );
+  //Разбить массив на чанки по 5 штук
+  const chunckSize = 5;
+  const errorItems: string[] = [];
+
+  for (let i: number = 0; i < fetchArray.length; i += chunckSize) {
+    //скопировать часть массива для передачи
+    let fetchedItems = fetchArray.slice(i, i + chunckSize);
+    //console.log(fetchedItems);
+
+    await Promise.allSettled(fetchedItems).then((data) =>
+      data.forEach((item) => {
+        if (item.status === "rejected") {
+          // console.log(item.value.ok);
+          errorItems.push(item.status);
+        }
+      }),
+    );
+  }
+
+  isError = errorItems.length > 0;
   if (isError) {
+    console.log("Error send status --- ", errorItems);
+
     return NextResponse.json({ ok: false });
   }
 
